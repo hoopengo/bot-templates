fbc88a80a85d65d6c7799c8259411171dc05bfe8
Aleksey <hoopengo@yandex.ru>
2023-02-23 01:55:07 +0300

    Update loader.py

--- bot/cogs/loader.py
+++ bot/cogs/loader.py
@@ -1,9 +1,11 @@
-from nextcord.ext.commands import Bot
+import nextcord.ext.commands as commands
 
 from .test import TestCog
 
-
-def register_all_cogs(bot: Bot) -> None:
+def register_all_cogs(bot: commands.Bot) -> None:
+    # Tuple containing all the cogs to register
     cogs = (TestCog,)
+
+    # Loop through each cog and register it with the bot
     for cog in cogs:
         bot.add_cog(cog(bot))
