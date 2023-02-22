a541b2cd2d478a4ab282f47d58ba9f5216280035
Aleksey <hoopengo@yandex.ru>
2023-02-23 00:29:46 +0300

    Update base.py

--- handlers/db (sql, if needed)/base.py
+++ handlers/db (sql, if needed)/base.py
@@ -1,11 +1,16 @@
+import logging
+
 from contextlib import contextmanager
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, declarative_base, sessionmaker
+from sqlalchemy.pool import QueuePool
 
-engine = create_engine("sqlite:///db.sqlite3", echo=True)
+engine = create_engine("sqlite:///db.sqlite3", echo=True, poolclass=QueuePool)
 Base = declarative_base(bind=engine)
 Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+logger = logging.getLogger(__name__)
 
 @contextmanager
 def session(**kwargs):
@@ -13,8 +18,9 @@ def session(**kwargs):
     try:
         yield new_session
         new_session.commit()
-    except:
+    except Exception as e:
         new_session.rollback()
+        logger.error(f"Error in session: {e}")
         raise
     finally:
         new_session.close()
